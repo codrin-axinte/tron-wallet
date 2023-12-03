b5b58cda4b1851fea174b198528ad6758875a588
Codrin Axinte <6564791+codrin-axinte@users.noreply.github.com>
2023-12-04 00:01:47 +0200

    fix transaction id

--- pages/api/transaction.js
+++ pages/api/transaction.js
@@ -1,15 +1,14 @@
-import createTron, {setupContract} from "@/lib/tronweb";
+import createTron from "@/lib/tronweb";
 
 
 export default async function handler(req, res) {
 
-    const {address} = req.query;
+    const {transaction} = req.query;
 
-// TODO: Research https://stackoverflow.com/questions/70647606/im-trying-to-fetch-all-transactions-of-a-certain-tron-address
     try {
         const tronWeb = createTron();
         //const transaction = await tronWeb.trx.getTransaction(txId);
-        const transactionInfo = await tronWeb.trx.getTransactionInfo(txId);
+        const transactionInfo = await tronWeb.trx.getTransactionInfo(transaction);
 
         res.status(200).json(transactionInfo);
     } catch (error) {
